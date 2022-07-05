b62973818d4b833e537105e011c3c585389168d0
cwangg897 <dhkdrb897@gmail.com>
2022-07-05 13:37:18 +0900

    [#1] 프로젝트 세팅

---
+++ src/main/java/com/flab/homedel/HomedelApplication.java
@@ -0,0 +1,13 @@
+package com.flab.homedel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomedelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomedelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flab/homedel/HomedelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.homedel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomedelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
